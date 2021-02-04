2d2650a6f92c46261f96fd3a40c93ef645989a5d
Yuri Ng <yuringuyen312@gmail.com>
2021-02-04 02:56:53 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -17,8 +17,11 @@ CONVENTIONAL_PRODUCE = [
 ]
 
 def assembled_matrix
-  matrix = [[CONVENTIONAL_PRODUCE],
-  [ORGANIC_PRODUCE]
+  matrix = [
+    ["Grapefruit", "Pineapple", "Oranges", "Watermelon", "Eggplant" ],
+    ["Strawberries","Potatoes","Grapes", "Avocadoes","Asparagus"],
+    ]
+
 
   ]
   # Build an array that contains both of the above arrays
